9f09b2d34b8ef787f4e8536455925a1563518fd8
Mira Torbay <118401965+mira-torbay@users.noreply.github.com>
2024-11-09 13:15:40 -0500

    added factor importance ranking and fixed data cleaning for caffeine and stress_levels

--- RF_model.py
+++ RF_model.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import StandardScaler
 
 # read .csv
 # load .csv and parse the timestamp as a datetime
-data = pd.read_csv("example.csv", parse_dates=['timestamp'])
+data = pd.read_csv("example_random.csv", parse_dates=['timestamp'])
 
 # set the timestamp as the index
 data.set_index('timestamp', inplace=True)
@@ -18,8 +18,12 @@ data['day_of_week'] = data.index.dayofweek  # extract day of the week
 
 # clean data
 data = data.resample('30T').asfreq()  # resample to 30 minutes
-data['sleep_duration'] = data['sleep_duration'].fillna(method='ffill')  # for sleep duration, replace missing value with most recent
+data['sleep_duration'] = data['sleep_duration'].fillna(method='ffill')  # for sleep duration, caffeine and stress level, replace missing value with most recent
+data['caffeine'] = data['caffeine'].fillna(method='ffill')
+data['stress_level'] = data['stress_level'].fillna(method='ffill')
 data = data.dropna(subset=['sleep_duration'])  # drop rows if NaNs still present (no value to use for ffill)
+data = data.dropna(subset=['caffeine'])
+data = data.dropna(subset=['stress_level'])
 data = data.apply(lambda col: col.fillna((col.shift(1) + col.shift(-1)) / 2) if col.name != 'sleep_duration' else col, axis=0)  # for everything else, take mean of next and prev values
 
 
@@ -53,3 +57,19 @@ result['timestamp'] = X_test.index
 result['predicted_stress_level'] = predictions
 
 print(result[['timestamp', 'heart_rate', 'sleep_duration', 'skin_temp', 'caffeine', 'blood_oxygen', 'predicted_stress_level']])
+
+# get feature importances
+importances = model.feature_importances_
+
+# create a DataFrame with each factor and it's corresponding importance (relative effect on stress)
+factor_importances = pd.DataFrame({
+    'Factor': X.columns,
+    'Importance': importances
+})
+
+# Sort the DataFrame by importance in descending order
+factor_importances = factor_importances.sort_values(by='Importance', ascending=False)
+
+# Print the ranked factors
+print("Ranking of factors by effect on stress level:")
+print(factor_importances)
